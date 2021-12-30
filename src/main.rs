5b4973c45dc696bca6eefc4a479ec988ff17d53b
Thierry Kühni <thierry.kuehni@students.fhnw.ch>
2021-12-30 16:32:28 +0100

    update to new ll-api, added tests to verify target stack shield hardware

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,22 @@ extern crate ll_api;
 extern crate rppal;
 extern crate shared_bus;
 
-use std::path::Path;
 use std::sync::Mutex;
-use std::time::Duration;
 
-use ll_api::TargetStackShield;
-use ll_api::{Probe, Target};
+use ll_api::{RpiTestChannel, TargetStackShield, TestChannel};
+use ll_api::{Target, TestInputPin};
 use pca9535::IoExpander;
 use pca9535::Pca9535Immediate;
+use rppal::gpio::Gpio;
 use rppal::i2c::I2c;
-use rppal::uart::{Parity, Uart};
 
 const ADDR: u8 = 32;
 
 fn main() {
     let i2c = I2c::new().unwrap();
+    let mut gpio = Gpio::new().unwrap();
 
     let i2c_bus: &'static _ = shared_bus::new_std!(I2c = i2c).unwrap();
-
-    let mut uart = Uart::with_path(Path::new("/dev/ttyAMA0"), 115200, Parity::None, 8, 1).unwrap();
-    uart.set_read_mode(1, Duration::from_millis(500)).unwrap();
-    uart.set_write_mode(true).unwrap();
 
     let expander = Pca9535Immediate::new(i2c_bus.acquire_i2c(), ADDR);
     let io_expander: IoExpander<Mutex<_>, _> = IoExpander::new(expander);
@@ -31,23 +26,90 @@ fn main() {
 
     shield.init_pins().unwrap();
 
-    for i in 1..8 {
-        let expanderx = Pca9535Immediate::new(i2c_bus.acquire_i2c(), ADDR + i);
-        let io_expanderx: IoExpander<Mutex<_>, _> = IoExpander::new(expanderx);
+    let mut test_channels: [Option<RpiTestChannel>; 4] = [None, None, None, None];
+    let test_channel_enum = [
+        TestChannel::Channel0,
+        TestChannel::Channel1,
+        TestChannel::Channel2,
+        TestChannel::Channel3,
+    ];
 
-        let mut shieldx = TargetStackShield::new(&io_expanderx);
-        shieldx.init_pins().unwrap();
+    let target_enum = [
+        Target::Target0,
+        Target::Target1,
+        Target::Target2,
+        Target::Target3,
+    ];
+
+    for i in 0..4 {
+        test_channels[i] = Some(RpiTestChannel::new(test_channel_enum[i]));
+
+        match test_channels[i] {
+            Some(ref mut channel) => channel.init_pins(&mut gpio).unwrap(),
+            None => panic!("Terstchannel {} not initialized", i),
+        }
     }
 
     if shield.daughterboard_is_connected().unwrap() {
-        shield
-            .connect_probe_to_target(Probe::Probe0, Target::Target0)
-            .unwrap();
+        for target_n in 0..4 {
+            for channel_n in 0..4 {
+                shield
+                    .connect_test_channel_to_target(
+                        test_channel_enum[channel_n],
+                        target_enum[target_n],
+                    )
+                    .unwrap();
 
-        let mut buf: [u8; 1] = [0; 1];
-        uart.write(&[5]).unwrap();
-        uart.read(&mut buf).unwrap();
+                let channel = test_channels[channel_n].as_mut().unwrap();
+                println!("Target no: {}, channel no: {}", target_n, channel_n);
+                println!("Checking gpio initialization");
 
-        println!("received from uart: {}", buf[0]);
+                assert!(!channel.test_input_is_high(TestInputPin::Pin0).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin1).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin2).unwrap());
+
+                println!("Checking UART connection");
+
+                channel.test_bus_write(&[5]).unwrap();
+
+                assert_eq!(channel.test_bus_read().unwrap()[0], 243);
+
+                println!("Checking Test GPIO");
+
+                channel.test_bus_write(&[0]).unwrap();
+                assert!(channel.test_input_is_high(TestInputPin::Pin0).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin1).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin2).unwrap());
+
+                channel.test_bus_write(&[10]).unwrap();
+                assert!(!channel.test_input_is_high(TestInputPin::Pin0).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin1).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin2).unwrap());
+
+                channel.test_bus_write(&[1]).unwrap();
+                assert!(!channel.test_input_is_high(TestInputPin::Pin0).unwrap());
+                assert!(channel.test_input_is_high(TestInputPin::Pin1).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin2).unwrap());
+
+                channel.test_bus_write(&[10]).unwrap();
+                assert!(!channel.test_input_is_high(TestInputPin::Pin0).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin1).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin2).unwrap());
+
+                channel.test_bus_write(&[2]).unwrap();
+                assert!(!channel.test_input_is_high(TestInputPin::Pin0).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin1).unwrap());
+                assert!(channel.test_input_is_high(TestInputPin::Pin2).unwrap());
+
+                channel.test_bus_write(&[10]).unwrap();
+                assert!(!channel.test_input_is_high(TestInputPin::Pin0).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin1).unwrap());
+                assert!(!channel.test_input_is_high(TestInputPin::Pin2).unwrap());
+
+                channel.test_output_set_high().unwrap();
+                assert_eq!(channel.test_bus_read().unwrap()[0], 4);
+                channel.test_output_set_low().unwrap();
+            }
+        }
     }
 }
